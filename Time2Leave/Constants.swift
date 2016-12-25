9fd15468806cb194b64b2e01bd868a546379243c
André Brinkop <andre@brinkop.de>
2016-12-25 23:07:12 +0100

    Add API-Key management

---
+++ Time2Leave/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Time2Leave
+//
+//  Created by André Brinkop on 25.12.16.
+//  Copyright © 2016 André Brinkop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    // MARK: API Keys
+    
+    private struct apiKeyNames {
+
+    }
+    
+    struct apiKeys {
+        static let plistName = "ApiKeys"
+    }
+}
