ac8433d85aadfe6be6b971a146789f037634e01d
André Brinkop <andre@brinkop.de>
2016-12-26 15:45:15 +0100

    Add table view and search bar (searching for locations using the GooglePlaces client) to initial view controller

--- Time2Leave/ViewController.swift
+++ Time2Leave/ViewController.swift
@@ -7,17 +7,110 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+    
+    // TODO: Remove Dummy Data
+    var dummyLocation = CLLocationCoordinate2D(latitude: 52.373715, longitude: 9.731253)
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tableViewSectionCount = 2
+    var autocompleteLocations = [Location]()
+    let autocompleteSection = 0
+    let autocompleteSectionName = "Search Results"
+    var favoriteLocations = [Location]()
+    let favoriteSection = 1
+    let favoriteSectionName = "Favorite Locations"
+    
+    var showNoResultsCell: Bool {
+        return searchController.isActive && autocompleteLocations.count == 0 && searchController.searchBar.text! != ""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO: Remove Dummy Data
+        favoriteLocations.append(Location(description: "favorite", id: "1"))
+        favoriteLocations.append(Location(description: "location", id: "2"))
+
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        let input = searchController.searchBar.text!
+        
+        GooglePlacesClient.autocomplete(input: input, location: dummyLocation) { locations, error in
+            guard error == nil else {
+                // TODO: Handle Error
+                print(error!)
+                return
+            }
+            
+            self.autocompleteLocations = locations!
+            self.tableView.reloadData()
+        }
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if self.tableView(tableView, numberOfRowsInSection: section) == 0 {
+            return nil
+        }
+        
+        switch section {
+        case autocompleteSection:
+            return autocompleteSectionName
+        case favoriteSection:
+            return favoriteSectionName
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case autocompleteSection:
+            if showNoResultsCell {
+                return 1
+            }
+            return autocompleteLocations.count
+        case favoriteSection:
+            return favoriteLocations.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if showNoResultsCell && indexPath.section == autocompleteSection {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "noResultsCell")!
+            cell.textLabel?.text = "No Results"
+            return cell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell")!
+        
+        switch indexPath.section {
+        case autocompleteSection:
+            cell.textLabel?.text = autocompleteLocations[indexPath.row].description
+        case favoriteSection:
+            cell.textLabel?.text = favoriteLocations[indexPath.row].description
+        default:
+            cell.textLabel?.text = ""
+        }
+    
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return tableViewSectionCount
     }
 
 
